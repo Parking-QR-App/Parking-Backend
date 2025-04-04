2756061f74d248cd02dcd90badada7261d366e0b
Riyaansh Mittal <riyaanshmittal14@gmail.com>
2025-04-04 18:20:10 +0530

    Update database

--- scanQR/settings.py
+++ scanQR/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'scanQR.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'parking',
+        'USER': 'riyaansh',
+        'PASSWORD': 'Riyaan$hM07',
+        'HOST': 'parking-db.chas2ao8emec.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
